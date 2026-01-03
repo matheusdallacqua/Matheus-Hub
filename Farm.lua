f03519e268cf1b359fd51b40aca99e7e91e06175
matheusdallacqua <matheusdallacqua1507@gmail.com>
2026-01-03 16:28:38 -0300

    Implement FastAttack function in FarmModule
    Added FastAttack function to enable fast attack mode.

--- Farm.lua
+++ Farm.lua
@@ -1,1 +1,11 @@
-
+--Fast attack
+function FarmModule.FastAttack(state)
+    _G.FastAttack = state
+    task.spawn(function()
+        while _G.FastAttack do
+            -- Ele vai usar o número que o loop do Main.lua converteu
+            task.wait(_G.FastAttackDelay or 0.1) 
+            -- ... resto do código de ataque ...
+        end
+    end)
+end
