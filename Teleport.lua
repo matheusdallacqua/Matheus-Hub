db0b3fd75d9695152e45fc70702718f60f7e84fa
matheusdallacqua <matheusdallacqua1507@gmail.com>
2026-01-03 13:19:56 -0300

    Update island coordinates in TeleportModule

--- Teleport.lua
+++ Teleport.lua
@@ -1,36 +1,43 @@
 local TeleportModule = {}
 
--- [[ COORDENADAS DO MATHEUS HUB ]]
+-- [[ TABELA DE ILHAS COMPLETA - ESTILO HOHO/TSUO ]]
 TeleportModule.Islands = {
     ["Sea 1"] = {
-        ["Starter Island"] = CFrame.new(973, 25, 1413),
-        ["Jungle"] = CFrame.new(-1612, 50, 147),
-        ["Desert"] = CFrame.new(944, 25, 4373),
-        ["Middle Town"] = CFrame.new(-690, 25, 1583),
-        ["Prison"] = CFrame.new(4875, 25, 734),
-        ["Magma Village"] = CFrame.new(-5244, 25, 8486),
-        ["Fountain City"] = CFrame.new(5127, 70, 4105)
+        ["Starter Island"] = CFrame.new(973, 16, 1413),
+        ["Jungle"] = CFrame.new(-1612, 37, 147),
+        ["Desert"] = CFrame.new(944, 16, 4373),
+        ["Middle Town"] = CFrame.new(-690, 15, 1583),
+        ["Frozen Village"] = CFrame.new(1129, 5, -1149),
+        ["Marineford"] = CFrame.new(-5029, 28, 4310),
+        ["Skypiea"] = CFrame.new(-4839, 717, -2622),
+        ["Prison"] = CFrame.new(4875, 16, 734),
+        ["Magma Village"] = CFrame.new(-5244, 12, 8486),
+        ["Fountain City"] = CFrame.new(5127, 59, 4105)
     },
     ["Sea 2"] = {
-        ["Cafe"] = CFrame.new(-382, 85, 255), -- Café Adicionado
-        ["Kingdom of Rose"] = CFrame.new(-451, 85, 1727),
-        ["Green Zone"] = CFrame.new(-2448, 85, -253),
-        ["Graveyard"] = CFrame.new(-2322, 25, 3140),
-        ["Snow Mountain"] = CFrame.new(900, 50, 4400),
-        ["Cursed Ship"] = CFrame.new(923, 140, 32852),
-        ["Ice Castle"] = CFrame.new(6140, 310, -6742),
-        ["Forgotten Island"] = CFrame.new(-3033, 250, -10175)
+        ["Cafe"] = CFrame.new(-382, 73, 255),
+        ["Kingdom of Rose"] = CFrame.new(-451, 73, 1727),
+        ["Green Zone"] = CFrame.new(-2448, 73, -253),
+        ["Graveyard"] = CFrame.new(-2322, 10, 3140),
+        ["Snow Mountain"] = CFrame.new(900, 40, 4400),
+        ["Hot and Cold"] = CFrame.new(-6062, 16, -5003),
+        ["Cursed Ship"] = CFrame.new(923, 125, 32852),
+        ["Ice Castle"] = CFrame.new(6140, 294, -6742),
+        ["Forgotten Island"] = CFrame.new(-3033, 235, -10175),
+        ["Dark Arena"] = CFrame.new(3776, 15, -3498)
     },
     ["Sea 3"] = {
-        ["Mansion"] = CFrame.new(-12463, 350, -7541),
-        ["Port Town"] = CFrame.new(-780, 25, 5322),
-        ["Hydra Island"] = CFrame.new(5747, 630, -266),
-        ["Floating Turtle"] = CFrame.new(-13274, 550, -7583),
-        ["Castle on the Sea"] = CFrame.new(-5075, 330, -3153),
-        ["Haunted Castle"] = CFrame.new(-9547, 180, 5532),
-        ["Tiki Outpost"] = CFrame.new(-16234, 25, 485)
+        ["Mansion"] = CFrame.new(-12463, 332, -7541),
+        ["Port Town"] = CFrame.new(-780, 15, 5322),
+        ["Hydra Island"] = CFrame.new(5747, 613, -266),
+        ["Floating Turtle"] = CFrame.new(-13274, 531, -7583),
+        ["Castle on the Sea"] = CFrame.new(-5075, 314, -3153),
+        ["Haunted Castle"] = CFrame.new(-9547, 164, 5532),
+        ["Sea of Treats"] = CFrame.new(-9510, 164, -14150),
+        ["Tiki Outpost"] = CFrame.new(-16234, 12, 485)
     }
 }
+
 
 -- [[ FUNÇÃO DE VOO TSUO STYLE ]]
 function TeleportModule.ToPos(targetCFrame, state)
